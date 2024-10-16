f96559b2d275cca939d0af867b5aeb4ea4f6713a
Christoph Neuner <19358803+ChristophNeuner@users.noreply.github.com>
2024-10-16 18:13:17 +0200

    added tests

--- test/adap-b01/names/Name.test.ts
+++ test/adap-b01/names/Name.test.ts
@@ -9,15 +9,28 @@ describe("Basic initialization tests", () => {
 });
 
 describe("Basic function tests", () => {
-  it("test insert", () => {
+  it("test functions", () => {
     let n: Name = new Name(["oss", "fau", "de"]);
     n.insert(1, "cs");
     expect(n.asNameString()).toBe("oss.cs.fau.de");
+
+    n.remove(1);
+    expect(n.asNameString()).toBe("oss.fau.de");
+
+    n.append("people");
+    expect(n.asNameString()).toBe("oss.fau.de.people");
+
+    n.setComponent(1, "cs");
+    expect(n.asNameString()).toBe("oss.cs.de.people");
+
+    expect(n.getComponent(1)).toBe("cs");
+
+    expect(n.getNoComponents()).toBe(4);
   });
 });
 
 describe("Delimiter function tests", () => {
-  it("test insert", () => {
+  it("test other delimiter", () => {
     let n: Name = new Name(["oss", "fau", "de"], '#');
     n.insert(1, "cs");
     expect(n.asNameString()).toBe("oss#cs#fau#de");
@@ -31,5 +44,19 @@ describe("Escape character extravaganza", () => {
     expect(n.asNameString()).toBe("oss.cs.fau.de");
     n.append("people");
     expect(n.asNameString()).toBe("oss.cs.fau.de#people");
+  });
+});
+
+describe("input checking", () => {
+  it("error handling", () => {
+    let n: Name = new Name(["oss", "fau", "de"]);
+    expect(() => n.getComponent(-1)).toThrow("Index out of bounds");
+    expect(() => n.getComponent(3)).toThrow("Index out of bounds");
+    expect(() => n.setComponent(-1, "cs")).toThrow("Index out of bounds");
+    expect(() => n.setComponent(3, "cs")).toThrow("Index out of bounds");
+    expect(() => n.insert(-1, "cs")).toThrow("Index out of bounds");
+    expect(() => n.insert(3, "cs")).toThrow("Index out of bounds");
+    expect(() => n.remove(-1)).toThrow("Index out of bounds");
+    expect(() => n.remove(3)).toThrow("Index out of bounds");
   });
 });
