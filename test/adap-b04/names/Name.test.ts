dbd0ff312a880a1cdc2412a93a90da6b786a3865
Christoph Neuner <19358803+ChristophNeuner@users.noreply.github.com>
2024-11-23 14:46:07 +0100

    B04: added scaffold for name tests

---
+++ test/adap-b04/names/Name.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+
+import { IllegalArgumentException } from "../../../src/adap-b04/common/IllegalArgumentException";
+import { Name } from "../../../src/adap-b04/names/Name";
+import { StringName } from "../../../src/adap-b04/names/StringName";
+import { StringArrayName } from "../../../src/adap-b04/names/StringArrayName";
+
+
+describe("AbstractName Tests", () => {
+    it("TODO", async () => {
+
+    });
+});
+
+describe("StrinArrayName Tests", () => {
+    it("TODO", async () => {
+        
+    });
+});
+
+describe("StrinName Tests", () => {
+    it("TODO", async () => {
+        
+    });
+});
