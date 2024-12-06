e3c11d5aea6a55fdc09ea2bf23d4c86fc6f89501
Christoph Neuner <19358803+ChristophNeuner@users.noreply.github.com>
2024-12-06 13:06:57 +0100

    copied b05 version of names into b06

---
+++ src/adap-b06/names/AbstractName.ts
@@ -0,0 +1,184 @@
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
+import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
+import { Name } from "./Name";
+
+export abstract class AbstractName implements Name {
+  protected delimiter: string = DEFAULT_DELIMITER;
+
+  constructor(delimiter: string = DEFAULT_DELIMITER) {
+    this.assertHasValidDelimiter(delimiter);
+    if (delimiter !== undefined) {
+      this.delimiter = delimiter;
+    } else {
+      this.delimiter = DEFAULT_DELIMITER;
+    }
+    this.assertIsValidDelimiter(delimiter);
+  }
+
+  // methods for assertions (class invariants)
+  protected assertAbstractNameIsValid() {
+    InvalidStateException.assertIsNotNullOrUndefined(this.delimiter);
+    this.assertIsValidDelimiter(this.delimiter);
+  }
+
+  public clone(): Name {
+    const cloned = Object.create(Object.getPrototypeOf(this));
+    cloned.delimiter = this.getDelimiterCharacter();
+
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      cloned.append(this.getComponent(i));
+    }
+
+    this.assertIsValidCloned(cloned);
+    return cloned;
+  }
+
+  public asString(delimiter?: string): string {
+    delimiter = delimiter ?? this.delimiter;
+    this.assertHasValidDelimiter(delimiter);
+
+    const components: string[] = [];
+
+    for (let i = 0; i < this.getNoComponents(); i++) {
+        const component = this.getComponent(i);   
+        components.push(component.replaceAll(`${ESCAPE_CHARACTER}${this.getDelimiterCharacter()}`, this.getDelimiterCharacter()));
+    }
+
+    return components.join(delimiter);
+}
+  
+  public toString(): string {
+    return this.asDataString();
+  }
+
+  public asDataString(): string {
+    let components: string[] = [];
+    const escapeWithDelimiter = ESCAPE_CHARACTER + this.getDelimiterCharacter();
+
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      const component = this.getComponent(i)
+        .replaceAll(this.getDelimiterCharacter(), escapeWithDelimiter);
+      components.push(component);
+    }
+
+    const name = components.join(this.getDelimiterCharacter());
+
+    // Return a JSON-encoded object
+    const data = {
+      dataString: name,
+      delimiter: this.getDelimiterCharacter(),
+    };
+
+    return JSON.stringify(data); // To ensure it's a machine-readable JSON string
+  }
+
+  public isEqual(other: Name): boolean {
+    this.assertHasValidParameter(other, "other cannot be null or undefined");
+    if (this.getNoComponents() !== other.getNoComponents()) return false;
+
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      if (this.getComponent(i) !== other.getComponent(i)) return false;
+    }
+    this.assertIsValidHashCode(other);
+    return true;
+  }
+
+  public getHashCode(): number {
+    let hashCode: number = 0;
+    const s: string = this.asDataString();
+    for (let i = 0; i < s.length; i++) {
+      let c = s.charCodeAt(i);
+      hashCode = (hashCode << 5) - hashCode + c;
+      hashCode |= 0;
+    }
+    return hashCode;
+  }
+
+  public isEmpty(): boolean {
+    return this.getNoComponents() === 0;
+  }
+
+  public getDelimiterCharacter(): string {
+    return this.delimiter;
+  }
+
+  abstract getNoComponents(): number;
+
+  abstract getComponent(i: number): string;
+  abstract setComponent(i: number, c: string): void;
+
+  abstract insert(i: number, c: string): void;
+  abstract append(c: string): void;
+  abstract remove(i: number): void;
+
+  public concat(other: Name): void {
+    this.assertHasValidParameter(other, "other cannot be null or undefined");
+    if (other.getDelimiterCharacter() !== this.getDelimiterCharacter()) {
+      throw new Error("Delimiters do not match");
+    }
+
+    let copy = Object.create(Object.getPrototypeOf(this));
+    copy.delimiter = this.getDelimiterCharacter();
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      copy.append(this.getComponent(i));
+    }
+
+    for (let i = 0; i <= other.getNoComponents(); ++i) {
+      this.append(other.getComponent(i));
+    }
+
+    this.assertIsValidConcatComponent(copy, other);
+  }
+
+  // methods for assertions (preconditions)
+  protected assertHasValidDelimiter(delimiter: string): void {
+    InvalidStateException.assertIsNotNullOrUndefined(
+      delimiter,
+      "delimiter cannot be null or undefined"
+    );
+    const cond = delimiter.length === 1 && delimiter !== ESCAPE_CHARACTER;
+    InvalidStateException.assertCondition(
+      cond,
+      "delimiter must be a single char and cannot be the escape character"
+    );
+  }
+
+  protected assertHasValidParameter(
+    o: Object | null,
+    msg: string = "null or undefined"
+  ): void {
+    InvalidStateException.assertIsNotNullOrUndefined(o, msg);
+  }
+
+  // methods for assertions (post-conditions)
+  protected assertIsValidDelimiter(delimiter: string | undefined): void {
+    const cond = (delimiter ? delimiter : DEFAULT_DELIMITER) === this.delimiter;
+    MethodFailedException.assertCondition(cond, "Name validation failed");
+  }
+
+  protected assertIsValidCloned(cloned: Name): void {
+    MethodFailedException.assertIsNotNullOrUndefined(
+      cloned,
+      "clone is null or undefined"
+    );
+    const cond = this.isEqual(cloned) && this !== cloned;
+    MethodFailedException.assertCondition(cond, "Clone validation failed");
+  }
+
+  protected assertIsValidHashCode(other: Name): void {
+    const cond = other.getHashCode() === this.getHashCode();
+    MethodFailedException.assertCondition(cond, "HashCode validation failed");
+  }
+
+  protected assertIsValidConcatComponent(copy: Name, other: Name): void {
+    const cond =
+      this.getNoComponents() ===
+      copy.getNoComponents() + other.getNoComponents();
+    MethodFailedException.assertCondition(
+      cond,
+      "Concat Components validation failed"
+    );
+  }
+}
---
+++ src/adap-b06/names/StringArrayName.ts
@@ -0,0 +1,178 @@
+import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
+import { Name } from "./Name";
+import { AbstractName } from "./AbstractName";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+
+export class StringArrayName extends AbstractName {
+  protected components: string[] = [];
+
+  constructor(other: string[], delimiter?: string) {
+    super(delimiter);
+    this.assertHasValidParameter(other, "other cannot be null or undefined");
+    this.init(other);
+    this.assertIsValidStringArrayNameState(other);
+  }
+
+  public createOrigin(): Name {
+    return new StringArrayName([""], DEFAULT_DELIMITER);
+  }
+
+  public init(other: string[]) {
+    this.components = [...other];
+  }
+
+  // methods for assertions (class invariants)
+  protected assertStringArrayNameisValid() {
+    super.assertAbstractNameIsValid();
+    InvalidStateException.assertIsNotNullOrUndefined(
+      this.components,
+      "Components cannot be null or undefined"
+    );
+
+    this.components.forEach((c) => {
+      InvalidStateException.assertIsNotNullOrUndefined(
+        c,
+        "A component in components is null or undefined"
+      );
+    });
+  }
+
+  public getNoComponents(): number {
+    return this.components.length;
+  }
+
+  public getComponent(i: number): string {
+    this.assertHasValidIndex(i);
+    return this.components[i];
+  }
+
+  public setComponent(i: number, c: string) {
+    this.assertHasValidIndex(i);
+    this.assertHasValidParameter(c);
+
+    const original = [...this.components];
+    this.components[i] = c;
+
+    this.assertIsValidComponent("set", c, i, original, original.length);
+  }
+
+  public insert(i: number, c: string) {
+    this.assertHasValidIndexForInsert(i);
+    this.assertHasValidParameter(c);
+
+    const original = [...this.components];
+    this.components.splice(i, 0, c);
+
+    this.assertIsValidComponent("insert", c, i, original, original.length);
+  }
+
+  public append(c: string) {
+    this.assertHasValidParameter(c);
+
+    const original = [...this.components];
+    this.components.push(c);
+
+    this.assertIsValidComponent("append", c, null, original, original.length);
+  }
+
+  public remove(i: number) {
+    this.assertHasValidIndex(i);
+
+    const original = [...this.components];
+    this.components.splice(i, 1);
+
+    this.assertIsValidComponent("remove", null, i, original, original.length);
+  }
+
+  // methods for assertions (preconditions)
+  protected assertHasValidIndex(i: number): void {
+    IllegalArgumentException.assertIsNotNullOrUndefined(i);
+    const cond = i >= 0 && i < this.getNoComponents();
+    IllegalArgumentException.assertCondition(cond, "Index is out of bounds");
+  }
+
+  protected assertHasValidIndexForInsert(i: number): void {
+    IllegalArgumentException.assertIsNotNullOrUndefined(i);
+    const cond = i >= 0 && i <= this.getNoComponents();
+    IllegalArgumentException.assertCondition(
+      cond,
+      "Index is out of bounds for insertion"
+    );
+  }
+
+  // methods for assertions (post-conditions)
+  protected assertIsValidStringArrayNameState(components: String[]): void {
+    const cond = components.every(
+      (comp, index) => comp === this.components[index]
+    );
+    MethodFailedException.assertCondition(
+      cond,
+      "StringArrayName validation failed"
+    );
+  }
+
+  protected assertIsValidComponent(
+    operationType: "set" | "insert" | "append" | "remove",
+    component: string | null,
+    index: number | null,
+    originalComponents: string[],
+    originalNoComponents: number
+  ): void {
+    const expectedNoComponents =
+      operationType === "set"
+        ? originalNoComponents
+        : operationType === "insert" || operationType === "append"
+        ? originalNoComponents + 1
+        : operationType === "remove"
+        ? originalNoComponents - 1
+        : originalNoComponents;
+
+    if (this.getNoComponents() !== expectedNoComponents) {
+      this.components = [...originalComponents];
+      MethodFailedException.assertCondition(
+        false,
+        `Component operation "${operationType}" failed: Invalid component count`
+      );
+    }
+
+    for (
+      let i_orig = 0, i_new = 0;
+      i_orig < originalNoComponents;
+      i_orig++, i_new++
+    ) {
+      if (operationType === "insert" && i_orig === index) {
+        if (this.components[i_new] !== component) {
+          this.components = [...originalComponents];
+          MethodFailedException.assertCondition(
+            false,
+            "Insert validation failed"
+          );
+        }
+        i_new++; // Springe in der neuen Liste wetier
+      } else if (operationType === "remove" && i_orig === index) {
+        i_orig++; // Überspringe den removed Index ind er alten Liste
+      } else if (operationType === "set" && i_orig === index) {
+        if (this.components[i_new] !== component) {
+            this.components = [...originalComponents];
+            MethodFailedException.assertCondition(false, "Set validation failed");
+        }
+      } else if (this.components[i_new] !== originalComponents[i_orig]) {
+        this.components = [...originalComponents];
+        MethodFailedException.assertCondition(
+          false,
+          `Component operation "${operationType}" failed: Component mismatch`
+        );
+      }
+    }
+
+    if (
+      operationType === "append" &&
+      this.components[originalNoComponents] !== component
+    ) {
+      this.components = [...originalComponents];
+      MethodFailedException.assertCondition(false, "Append validation failed");
+    }
+  }
+}
